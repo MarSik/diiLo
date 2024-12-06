f0945b61116a94c62ad999ac00c03dcbc9c45a95
Martin Sivak <mars@montik.net>
2024-12-06 13:59:30 +0100

    Store module file reorganization

---
+++ src/store/conversions.rs
@@ -0,0 +1,257 @@
+use chrono::{DateTime, Local};
+
+use super::{LedgerEntry, LedgerEntryDto, LedgerEvent};
+
+impl From<&LedgerEntryDto> for LedgerEntry {
+    fn from(val: &LedgerEntryDto) -> LedgerEntry {
+        //The parse_datetime call takes 5 ms and is extremely slow!
+        //let t = self.time.clone().map(parse_datetime).unwrap().unwrap();
+        let t = val
+            .time
+            .as_deref()
+            .map(DateTime::parse_from_rfc3339)
+            .unwrap_or_else(|| Ok(Local::now().fixed_offset()))
+            .unwrap();
+
+        if val.cmd_store {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::StoreTo(val.location_id.clone().unwrap().into()),
+            }
+        } else if val.cmd_take {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::TakeFrom(val.location_id.clone().unwrap().into()),
+            }
+        } else if val.cmd_deliver {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::DeliverFrom(
+                    val.source_id
+                        .as_ref()
+                        .or(val.location_id.as_ref())
+                        .unwrap()
+                        .as_str()
+                        .into(),
+                ),
+            }
+        } else if val.cmd_return {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::ReturnTo(
+                    val.source_id
+                        .as_ref()
+                        .or(val.location_id.as_ref())
+                        .unwrap()
+                        .as_str()
+                        .into(),
+                ),
+            }
+        } else if val.cmd_order {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::OrderFrom(
+                    val.source_id
+                        .as_ref()
+                        .or(val.location_id.as_ref())
+                        .unwrap()
+                        .as_str()
+                        .into(),
+                ),
+            }
+        } else if val.cmd_cancel_order {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::CancelOrderFrom(
+                    val.source_id
+                        .as_ref()
+                        .or(val.location_id.as_ref())
+                        .unwrap()
+                        .as_str()
+                        .into(),
+                ),
+            }
+        } else if val.cmd_require && val.location_id.is_some() {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::RequireIn(val.location_id.clone().unwrap().into()),
+            }
+        } else if val.cmd_require && val.project_id.is_some() {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::RequireInProject(val.project_id.clone().unwrap().into()),
+            }
+        } else if val.cmd_require && val.source_id.is_some() {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::OrderFrom(val.source_id.clone().unwrap().into()),
+            }
+        } else if val.cmd_solder {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::SolderTo(
+                    val.project_id
+                        .as_ref()
+                        .or(val.location_id.as_ref())
+                        .unwrap()
+                        .as_str()
+                        .into(),
+                ),
+            }
+        } else if val.cmd_unsolder {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::UnsolderFrom(
+                    val.project_id
+                        .as_ref()
+                        .or(val.location_id.as_ref())
+                        .unwrap()
+                        .as_str()
+                        .into(),
+                ),
+            }
+        } else if val.cmd_set {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::ForceCount(val.location_id.clone().unwrap().into()),
+            }
+        } else {
+            LedgerEntry {
+                t,
+                count: val.count,
+                part: val.part_id.as_str().into(),
+                ev: LedgerEvent::TakeFrom(val.location_id.clone().unwrap().into()),
+            }
+        }
+    }
+}
+
+impl From<&LedgerEntry> for LedgerEntryDto {
+    fn from(entry: &LedgerEntry) -> Self {
+        match &entry.ev {
+            LedgerEvent::TakeFrom(location) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                location_id: Some(location.to_string()),
+                cmd_take: true, // TODO check if location is a project -> unsolder
+                ..Default::default()
+            },
+            LedgerEvent::StoreTo(location) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                location_id: Some(location.to_string()),
+                cmd_store: true, // TODO check if location is a project -> solder
+                ..Default::default()
+            },
+            LedgerEvent::UnsolderFrom(location) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                location_id: Some(location.to_string()),
+                cmd_unsolder: true,
+                ..Default::default()
+            },
+            LedgerEvent::SolderTo(location) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                location_id: Some(location.to_string()),
+                cmd_solder: true,
+                ..Default::default()
+            },
+            LedgerEvent::ForceCount(location) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                location_id: Some(location.to_string()),
+                cmd_set: true,
+                ..Default::default()
+            },
+            LedgerEvent::RequireIn(location) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                location_id: Some(location.to_string()),
+                cmd_require: true,
+                ..Default::default()
+            },
+            LedgerEvent::RequireInProject(project_id) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                project_id: Some(project_id.to_string()),
+                cmd_require: true,
+                ..Default::default()
+            },
+            LedgerEvent::ReturnTo(source_id) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                source_id: Some(source_id.to_string()),
+                cmd_return: true,
+                ..Default::default()
+            },
+            LedgerEvent::DeliverFrom(source) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                source_id: Some(source.to_string()),
+                cmd_deliver: true,
+                ..Default::default()
+            },
+            LedgerEvent::OrderFrom(source) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                source_id: Some(source.to_string()),
+                cmd_order: true,
+                ..Default::default()
+            },
+            LedgerEvent::CancelOrderFrom(source) => LedgerEntryDto {
+                time: Some(entry.t.to_rfc3339()),
+                transaction: None,
+                count: entry.count,
+                part_id: entry.part.to_string(),
+                source_id: Some(source.to_string()),
+                cmd_cancel_order: true,
+                ..Default::default()
+            },
+        }
+    }
+}
---
+++ src/store/types.rs
@@ -0,0 +1,174 @@
+use std::{collections::HashSet, path::PathBuf, rc::Rc};
+
+use chrono::{DateTime, FixedOffset};
+use multimap::MultiMap;
+
+use crate::store::serializer::{deserialize_labels, serialize_labels};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ObjectType {
+    Part,
+    Source,
+    Project,
+    Location,
+}
+
+#[derive(Debug, Clone, Default, serde::Deserialize, serde::Serialize)]
+pub struct PartMetadata {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<String>,
+
+    #[serde(default)]
+    pub name: String,
+
+    #[serde(alias = "mfgid")]
+    #[serde(skip_serializing_if = "String::is_empty")]
+    #[serde(default)]
+    pub manufacturer_id: String,
+
+    #[serde(skip_serializing_if = "String::is_empty")]
+    #[serde(default)]
+    pub manufacturer: String,
+
+    #[serde(default)]
+    #[serde(serialize_with = "serialize_labels")]
+    #[serde(deserialize_with = "deserialize_labels")]
+    pub labels: MultiMap<String, String>,
+
+    #[serde(default)]
+    #[serde(alias = "attrs", alias = "attr")]
+    #[serde(serialize_with = "serialize_labels")]
+    #[serde(deserialize_with = "deserialize_labels")]
+    pub attributes: MultiMap<String, String>,
+
+    #[serde(default)]
+    pub types: HashSet<ObjectType>,
+
+    #[serde(default)]
+    pub summary: String,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Part {
+    pub id: PartId,
+    pub filename: Option<PathBuf>,
+    pub metadata: PartMetadata,
+    pub content: String,
+}
+
+pub fn is_false(v: &bool) -> bool {
+    !*v
+}
+
+#[derive(Debug, Default, serde::Deserialize, serde::Serialize)]
+pub(crate) struct LedgerEntryDto {
+    #[serde(alias = "t")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(super) time: Option<String>,
+
+    #[serde(alias = "tx")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(super) transaction: Option<String>,
+
+    #[serde(alias = "n", alias = "c")]
+    pub(super) count: usize,
+
+    #[serde(rename = "part")]
+    #[serde(skip_serializing_if = "String::is_empty")]
+    pub(super) part_id: String,
+
+    #[serde(
+        rename = "location",
+        alias = "destination",
+        alias = "dst",
+        alias = "to"
+    )]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(super) location_id: Option<String>,
+
+    #[serde(rename = "project", alias = "proj")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(super) project_id: Option<String>,
+
+    #[serde(rename = "source", alias = "from", alias = "src", alias = "fr")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub(super) source_id: Option<String>,
+
+    #[serde(rename = "take", alias = "move", alias = "m", alias = "-")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_take: bool,
+
+    #[serde(rename = "store", alias = "receive", alias = "a", alias = "+")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_store: bool,
+
+    #[serde(rename = "require", alias = "req", alias = "?")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_require: bool,
+
+    #[serde(rename = "solder", alias = "s")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_solder: bool,
+
+    #[serde(rename = "unsolder", alias = "u")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_unsolder: bool,
+
+    #[serde(rename = "order", alias = "o")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_order: bool,
+
+    #[serde(rename = "cancel", alias = "co")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_cancel_order: bool,
+
+    #[serde(rename = "deliver", alias = "d")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_deliver: bool,
+
+    #[serde(rename = "return", alias = "ret", alias = "send")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_return: bool,
+
+    #[serde(rename = "correct", alias = "set", alias = "=")]
+    #[serde(skip_serializing_if = "is_false")]
+    #[serde(default)]
+    pub(super) cmd_set: bool,
+}
+
+pub type PartId = Rc<str>;
+pub type LocationId = PartId;
+pub type SourceId = Rc<str>;
+
+#[derive(Debug)]
+pub struct LedgerEntry {
+    pub t: DateTime<FixedOffset>,
+    pub count: usize,
+    pub part: PartId,
+    pub ev: LedgerEvent,
+}
+
+#[derive(Debug)]
+pub enum LedgerEvent {
+    TakeFrom(LocationId),
+    StoreTo(LocationId),
+    ForceCount(LocationId),
+    RequireIn(LocationId),
+    OrderFrom(SourceId),
+    CancelOrderFrom(SourceId),
+    DeliverFrom(SourceId),
+    ReturnTo(SourceId),
+    UnsolderFrom(LocationId),
+    SolderTo(LocationId),
+    RequireInProject(LocationId),
+}
